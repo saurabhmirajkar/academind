20a49218fbd6a2af462c108d1d38981df3620251
Saurabh Mirajkar <mirajkar46@gmail.com>
2023-09-16 17:09:37 +0530

    mid changes

---
+++ lib/gradient_container.dart
@@ -0,0 +1,28 @@
+import 'package:academind/styled_text.dart';
+import 'package:flutter/material.dart';
+
+const startAlignment = Alignment.topLeft;
+const endAlignment = Alignment.bottomRight;
+
+class GradientContainer extends StatelessWidget {
+   const GradientContainer({super.key});
+   
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            Colors.amber,
+            Colors.red
+          ],
+          begin: startAlignment,
+          end: endAlignment,
+        ),
+      ),
+      child: const Center(
+        child: StyledText(),
+      ),
+    );
+  }
+}
---
+++ lib/styled_text.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class StyledText extends StatelessWidget {
+  const StyledText({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text(
+      'Hello Saurabh!',
+      style: TextStyle(
+        color: Colors.black,
+        fontSize: 28,
+        fontWeight: FontWeight.bold,
+      ),
+    );
+  }
+}
